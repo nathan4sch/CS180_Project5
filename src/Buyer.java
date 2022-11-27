8d12fd348bb0cefba7f4107e86bd5631b8b2ff10
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2022-11-26 21:21:26 -0500

    Update Buyer.java

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,287 +1,138 @@
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
- * The interface in which users with Buyer accounts can access their information
- * Lists the marketplace where Buyers can purchase products
+ * Buyers class - contains all methods the buyers may use
  *
- * @version 24/11/2022
+ * @author Colin Wu
+ * @version 2022-3-11
  */
-public class MainBuyerFrame extends JComponent implements Runnable {
-    Socket socket;
-    BufferedReader bufferedReader;
-    PrintWriter printWriter;
-
-    String[] columnNames = {"Store", "Product Name", "Price"};
-    String[][] rowData = new String[1][];
-    //Used for testing until server works
-    String[][] dummyRowData = {{"Joe's couches", "Couch", "199.99"}, {"Jim's Chairs", "chair", "29.99"}};
-    String[][] dummyRowData2 = {{"Jim's Chairs", "chair", "29.99"}, {"Joe's couches", "Couch", "199.99"}};
-    DefaultTableModel tableModel;
-    JTable jTable;
-
-    JFrame mainBuyerFrame;
-    JSplitPane splitPane;
-    JPanel leftPanel;
-    JPanel rightPanel;
-    JScrollPane jScrollPane;
-    JButton viewCartButton;
-    JButton searchButton;
-    JTextField searchTextField = new JTextField(10);
-    JButton sortButton;
-    JButton reviewHistoryButton;
-    JButton manageAccountButton;
-    JButton logoutButton;
-
-    JPopupMenu tablePopupMenu;
-    JMenuItem addToCart;
-    JMenuItem moreDetails;
-
-    JPopupMenu sortPopupMenu;
-    JMenuItem sortByPrice;
-    JMenuItem sortByQuantity;
+public class Buyer {
+    private final String email; // Buyer email - This is the unique identifier (Cannot be changed)
+    private String password; // Account Password
+    private ArrayList<String> purchaseHistory; // ArrayList of purchase history
+    private ArrayList<String> cart; // Buyer shopping cart
 
     /**
-     *  The constructor of MainBuyerFrame
+     * Buyer constructor
      *
-     * @param socket The socket that connect this local machine with the server
-     */
-    public MainBuyerFrame (Socket socket) {
-        this.socket = socket;
+     * @param email           Buyer email
+     * @param password        Buyer password
+     * @param purchaseHistory Buyer ArrayList storing purchase history
+     * @param cart            Buyer ArrayList storing shopping cart items
+     **/
+    public Buyer(String email, String password, ArrayList<String> purchaseHistory, ArrayList<String> cart) {
+        this.email = email;
+        this.password = password;
+        if (purchaseHistory == null) { //creating account
+            this.purchaseHistory = new ArrayList<>();
+        } else {                        //signing in
+            this.purchaseHistory = showPurchaseHistory(email);
+        }
+        if (cart == null) {
+            ArrayList<String> temp = new ArrayList<>();
+            temp.add("x");
+            this.cart = temp;
+        } else {
+            this.cart = showItemsInCart(email);
+        }
     }
 
-    ActionListener popupItemListener = new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            JMenuItem choice = (JMenuItem) e.getSource();
-            if (choice == addToCart) {
-                //add to cart code
-            } else if (choice == moreDetails) {
-                //more details code
-            } else if (choice == sortByPrice) {
-                //tells Server which operation to perform
-                printWriter.println("Sort Price");
-                printWriter.flush();
-                try {
-                    //gets number of items
-                    int numItems = Integer.parseInt(bufferedReader.readLine());
-                    tableModel = updateTable(numItems);
-                    //resets Table for user
-                    jTable.setModel(tableModel);
-                    mainBuyerFrame.repaint();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-            } else if (choice == sortByQuantity) {
-                //Tells server the operation to perform
-                printWriter.println("Sort Quantity");
-                printWriter.flush();
-                try {
-                    //Gets number of items
-                    int numItems = Integer.parseInt(bufferedReader.readLine());
-                    tableModel = updateTable(numItems);
-                    //resets table for user
-                    jTable.setModel(tableModel);
-                    mainBuyerFrame.repaint();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-            }
-        }
-    };
-
-    ActionListener actionListener = new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            Object source = e.getSource();
-
-            //Main options from Right Panel
-            if (source == viewCartButton) { // (1) Select Product
-                printWriter.println("View Cart");
-                printWriter.flush();
-
-                try {
-                    String cartLine = bufferedReader.readLine();
-                    String[] buyerCarts = cartLine.split("~");
-
-                    SwingUtilities.invokeLater(new CartFrame(socket, buyerCarts));
-                    mainBuyerFrame.dispose();
-                } catch (Exception exc) {
-                    exc.printStackTrace();
-                }
-            } else if (source == searchButton) { // (3) Search
-                printWriter.println("Search");
-                //prints the text that the user is searching for to server
-                printWriter.println(searchTextField.getText());
-                printWriter.flush();
-                try {
-                    int numItems = Integer.parseInt(bufferedReader.readLine());
-                    tableModel = updateTable(numItems);
-                    jTable.setModel(tableModel);
-                    mainBuyerFrame.repaint();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-            } else if (source == manageAccountButton) { // (5) Manage Account
-
-            } else if (source == reviewHistoryButton) { // (6) View Statistics
-
-            } else if (source == logoutButton) { // (7) Sign Out
-                SwingUtilities.invokeLater(new LoginFrame(socket));
-                mainBuyerFrame.dispose();
-            }
-        }
-    };
-
-    public void run() {
+    /**
+     * Returns an ArrayList to be printed as the purchase history
+     *
+     * @param email Email to search for when adding to array list
+     */
+    public static ArrayList<String> showPurchaseHistory(String email) {
         try {
-            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            printWriter = new PrintWriter(socket.getOutputStream());
+            // Read through CSV file
+            BufferedReader purchasesReader = new BufferedReader(new FileReader("FMCredentials.csv"));
+
+            ArrayList<String> FMCredentials = new ArrayList<>();
+
+            // Add existing items to ArrayList;
+            String line = purchasesReader.readLine();
+            while (line != null) {
+                FMCredentials.add(line);
+                line = purchasesReader.readLine();
+            }
+
+            purchasesReader.close();
+
+            // loop through arraylist and find the correct account
+            for (int i = 0; i < FMCredentials.size(); i++) {
+                // If arraylist index has email
+                if (FMCredentials.get(i).contains(email)) {
+                    String[] strSplit = FMCredentials.get(i).split(",");
+                    String purchaseHistoryStr = strSplit[4];
+                    if (purchaseHistoryStr.equals("x")) {
+                        return new ArrayList<>();
+                    }
+                    String[] purchaseHistoryLine = purchaseHistoryStr.split("~");
+
+                    // Return new ArrayList
+                    return new ArrayList<>(Arrays.asList(purchaseHistoryLine));
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
+    }
 
-        mainBuyerFrame = new JFrame("Account Frame");
-        splitPane = new JSplitPane();
-        leftPanel = new JPanel();
-        rightPanel = new JPanel();
 
-        //configure splitPane
-        splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
-        splitPane.setDividerLocation(800);
-        splitPane.setLeftComponent(leftPanel);
-        splitPane.setRightComponent(rightPanel);
+    /**
+     * Returns an ArrayList of all the items in a buyer's cart
+     *
+     * @param email Email to search for when adding to array list
+     **/
+    public static ArrayList<String> showItemsInCart(String email) {
+        try {
+            BufferedReader cartReader = new BufferedReader(new FileReader("FMCredentials.csv"));
 
-        //rightPanel
-        rightPanel.setLayout(new GridLayout(5, 1, 20, 20));
+            ArrayList<String> FMCredentials = new ArrayList<>();
 
-        //Adds view Cart Button
-        viewCartButton = new JButton("View Cart");
-        viewCartButton.addActionListener(actionListener);
-        rightPanel.add(viewCartButton);
+            // Add existing items to ArrayList;
+            String line;
+            while ((line = cartReader.readLine()) != null) {
+                FMCredentials.add(line);
+            }
+            cartReader.close();
 
-        //Creates Sort button with popup menu functionality
-        sortPopupMenu = new JPopupMenu();
-        sortByPrice = new JMenuItem("Sort By Price");
-        sortByQuantity = new JMenuItem("Sort By Quantity");
-        sortByPrice.addActionListener(popupItemListener);
-        sortByQuantity.addActionListener(popupItemListener);
-        sortPopupMenu.add(sortByPrice);
-        sortPopupMenu.add(sortByQuantity);
-        sortButton = new JButton("Sort Dashboard");
-        sortButton.setComponentPopupMenu(sortPopupMenu);
-        rightPanel.add(sortButton);
+            for (int i = 0; i < FMCredentials.size(); i++) {
+                // If arraylist index has email
+                if (FMCredentials.get(i).contains(email)) {
+                    String[] strSplit = FMCredentials.get(i).split(",");
+                    String shoppingCartInfo = strSplit[4];
+                    String[] shoppingCartLine = shoppingCartInfo.split("~");
 
-        //adds review History Button
-        reviewHistoryButton = new JButton("Review Purchase History");
-        reviewHistoryButton.addActionListener(actionListener);
-        rightPanel.add(reviewHistoryButton);
-
-        //adds manage account button
-        manageAccountButton = new JButton("Manage Account");
-        manageAccountButton.addActionListener(actionListener);
-        rightPanel.add(manageAccountButton);
-
-        //adds logout button
-        logoutButton = new JButton("Logout");
-        logoutButton.addActionListener(actionListener);
-        rightPanel.add(logoutButton);
-
-        leftPanel.add(searchTextField);
-
-        searchButton = new JButton("Search");
-        searchButton.addActionListener(actionListener);
-        leftPanel.add(searchButton);
-
-        tablePopupMenu = new JPopupMenu();
-        addToCart = new JMenuItem("Add to Cart");
-        moreDetails = new JMenuItem("Delete Friend");
-
-        //Adds Popup Functionality to table
-        addToCart.addActionListener(popupItemListener);
-        moreDetails.addActionListener(popupItemListener);
-        tablePopupMenu.add(addToCart);
-        tablePopupMenu.add(moreDetails);
-        
-        //Initializes table of Items for user to view
-        printWriter.println("Initial Table");
-        int itemsInInitialTable = -1;
-//        try {
-//            itemsInInitialTable = Integer.parseInt(bufferedReader.readLine());
-//        } catch (IOException ioe) {
-//            ioe.printStackTrace();
-//        }
-        tableModel = updateTable(itemsInInitialTable);
-        jTable = new JTable(tableModel);
-        //adds popups to table
-        jTable.setComponentPopupMenu(tablePopupMenu);
-
-        //Makes the table scrollable
-        jScrollPane = new JScrollPane(jTable, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        leftPanel.add(jScrollPane);
-
-        //Finalize frame
-        mainBuyerFrame.add(splitPane);
-        mainBuyerFrame.setSize(1000, 800);
-        mainBuyerFrame.setLocationRelativeTo(null);
-        mainBuyerFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-        mainBuyerFrame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                try {
-                    bufferedReader.close();
-                    printWriter.close();
-                    socket.close();
-                    mainBuyerFrame.dispose();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
+                    return new ArrayList<>(Arrays.asList(shoppingCartLine));
                 }
             }
-        });
-        mainBuyerFrame.setVisible(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
-     * Returns sorted Table Model
-     * @param numItems how many rows the table will contain
-     * @return Updated Item Table
+     * gets user's cart
+     *
+     * @return cart ArrayList
      */
-    public DefaultTableModel updateTable(int numItems) {
-        if (numItems == 0) {
-            rowData = null;
-        } else if (numItems == -1) {
-            JOptionPane.showMessageDialog(null, "Failed to Fetch server Data",
-                    "Server Issue", JOptionPane.ERROR_MESSAGE);
-        } else {
-            rowData = new String[numItems][3];
-            try {
-                for (int i = 0; i < numItems; i++) {
-                    String store = bufferedReader.readLine();
-                    String productName = bufferedReader.readLine();
-                    String price = bufferedReader.readLine();
-                    rowData[i][0] = store;
-                    rowData[i][1] = productName;
-                    rowData[i][2] = price;
-                }
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-            return new DefaultTableModel(rowData, columnNames) {
-                public boolean isCellEditable(int row, int column) {
-                    return false;
-                }
-            };
-        }
-        return null;
+    public ArrayList<String> getCart() {
+        return cart;
+    }
+
+    /**
+     * gets user's purchase history
+     *
+     * @return purchaseHistory ArrayList
+     */
+    public ArrayList<String> getPurchaseHistory() {
+        return purchaseHistory;
     }
 }
